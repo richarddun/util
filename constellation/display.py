aaebb4a038bd8009d4bcc879a0708eecc3d22dc3
Richard Dunne <richard.w.dunne@gmail.com>
2016-03-18 00:10:31 +0000

    building counter selection flow

--- constellation/display.py
+++ constellation/display.py
@@ -20,31 +20,36 @@ class BaseWin(object):
         self.countpan = curses.panel.new_panel(self.win)
         self.infobpan = curses.panel.new_panel(self.infobox)
         self.infobpan.hide()#hiding it for now
-        
-    def Main_Cselect(self,topclist):
+
+    def Main_Cselect(self,topclist,countermap):
         self.context = 1 #initial selection context
         self.mlocptr = 1 #track index (current)
         self.prevloc = 1 #track index (previous)
         self.maxstrlen = 0
         self.upperclist = topclist
+        self.countmap = countermap
         self.mlocationref = {}
-        for cl in self.upperclist:
-            if len(cl) > self.maxstrlen:
-                self.maxstrlen = len(cl)#get longest stringlen from list
-        for index,cl in enumerate(self.upperclist,1):
-            self.mlocationref[index]=cl #remember for later
+        self.win.addstr(self.starty,self.startx,
+                'Please select a base counter:')
+        for countl in self.upperclist:
+            if len(countl) > self.maxstrlen:
+                self.maxstrlen = len(countl)
+                #get longest stringlen from list
+        
+        for index,countl in enumerate(self.upperclist,1):
+            self.mlocationref[index]=countl #remember for later
             if index == 1:
-                self.win.addstr(self.starty+index,self.startx,cl,
+                self.win.addstr(self.starty+index,self.startx,countl,
                     curses.A_REVERSE)
             else:
-                self.win.addstr(self.starty+index,self.startx,cl)
+                self.win.addstr(self.starty+index,self.startx,countl)
         self.win.refresh()
 
     def selectdown(self):
+        rightshift = 0
         if self.mlocptr + 1 > len(self.mlocationref.keys()):
         #if pressing down will send us off the bottom
         #go back to the top
-            self.prevloc = self.mlocptr
             self.win.addstr(self.starty+self.prevloc,self.startx,
                 self.mlocationref[self.prevloc])
             #unset the vid reverse on previous string
@@ -52,20 +57,34 @@ class BaseWin(object):
             self.win.addstr(self.starty+self.mlocptr,self.startx,
                 self.mlocationref[self.mlocptr],curses.A_REVERSE)
             #invert the text to highlight current selection
+           
+            self.prevloc = self.mlocptr
         else:
             self.mlocptr += 1
             self.win.addstr(self.starty+self.mlocptr,self.startx,
                 self.mlocationref[self.mlocptr],curses.A_REVERSE)
+            for index,counter in enumerate(
+                   self.countmap[
+                       self.mlocationref[
+                           self.mlocptr]],1):#need to go deeper
+               self.win.addstr(self.starty+self.prevloc+index,
+                       self.startx+self.maxstrlen+rightshift+3,counter)
+               if self.win.getyx()[0] - 5 > self.len_y - 5:
+                   rightshift += 5
             self.win.addstr(self.starty+self.prevloc,self.startx,
                 self.mlocationref[self.prevloc])
             self.prevloc = self.mlocptr
+        
+        
+        
         self.win.refresh()
+        
+
 
     def selectup(self):
         if (self.mlocptr - 1) < 1:
         #if pressing up will send us off the top
         #go back to the bottom
-            self.prevloc = self.mlocptr
             self.win.addstr(self.starty+self.prevloc,self.startx,
                 self.mlocationref[self.prevloc])
             #unset the vid reverse on previous string
@@ -73,6 +92,7 @@ class BaseWin(object):
             self.win.addstr(self.starty+self.mlocptr,self.startx,
                 self.mlocationref[self.mlocptr],curses.A_REVERSE)
             #invert the text to highlight current selection
+            self.prevloc = self.mlocptr
         else:
             self.mlocptr -= 1
             self.win.addstr(self.starty+self.mlocptr,self.startx,
@@ -80,7 +100,7 @@ class BaseWin(object):
             self.win.addstr(self.starty+self.prevloc,self.startx,
                 self.mlocationref[self.prevloc])
             self.prevloc = self.mlocptr
-       
+        self.win.refresh()
 
     def Sub_Cselect(self):
         pass
